6ce016c64509b51fd7418fb3917a54483b1420d7
61-Mime <a89520@alunos.uminho.pt>
2020-04-30 19:13:30 +0100

    Classe das encomendas aceites

--- TrabalhoPratico/src/com/company/Loja.java
+++ TrabalhoPratico/src/com/company/Loja.java
@@ -6,18 +6,20 @@ public class Loja {
 
     private String storeCode;
     private String storeName;
+    private Coordenadas gps;
     private boolean hasQueueInfo;
     private double queueTime;
 
     //--------------------------------------------------------------Construtores--------------------------------------------------------------------------\\
 
     public Loja() {
-        this("", "", false, -1);
+        this("", "", new Coordenadas(), false, -1);
     }
 
-    public Loja(String storeCode, String storeName, boolean hasQueueInfo, double queueTime) {
+    public Loja(String storeCode, String storeName, Coordenadas gps, boolean hasQueueInfo, double queueTime) {
         this.storeCode = storeCode;
         this.storeName = storeName;
+        this.gps = gps.clone();
         this.hasQueueInfo = hasQueueInfo;
         this.queueTime = queueTime;
     }
@@ -25,6 +27,7 @@ public class Loja {
     public Loja(Loja l) {
         this.storeCode = l.getStoreCode();
         this.storeName = l.getStoreName();
+        this.gps = l.getGps();
         this.hasQueueInfo = l.isHasQueueInfo();
         this.queueTime = l.getQueueTime();
     }
@@ -47,6 +50,15 @@ public class Loja {
         this.storeName = storeName;
     }
 
+     public Coordenadas getGps() {
+        return gps.clone();
+    }
+
+    public void setGps(Coordenadas gps) {
+        this.gps.setLongitude(gps.getLongitude());
+        this.gps.setLatitude(gps.getLatitude());
+    }
+
     public boolean isHasQueueInfo() {
         return hasQueueInfo;
     }
@@ -65,24 +77,31 @@ public class Loja {
 
     //--------------------------------------------------------------toString, equals e clone--------------------------------------------------------------------------\\
 
+
+    @Override
     public String toString() {
         final StringBuilder sb = new StringBuilder("Loja{");
         sb.append("storeCode='").append(storeCode).append('\'');
         sb.append(", storeName='").append(storeName).append('\'');
+        sb.append(", gps=").append(gps);
         sb.append(", hasQueueInfo=").append(hasQueueInfo);
         sb.append(", queueTime=").append(queueTime);
         sb.append('}');
         return sb.toString();
     }
 
+    @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
         Loja loja = (Loja) o;
         return hasQueueInfo == loja.hasQueueInfo &&
                 Double.compare(loja.queueTime, queueTime) == 0 &&
                 Objects.equals(storeCode, loja.storeCode) &&
-                Objects.equals(storeName, loja.storeName);
+                Objects.equals(storeName, loja.storeName) &&
+                Objects.equals(gps, loja.gps);
     }
 
     public Loja clone() {
