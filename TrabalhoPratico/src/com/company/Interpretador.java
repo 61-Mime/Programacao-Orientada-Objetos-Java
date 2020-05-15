ed48bd310b28339d7c3380d5fb77740a404e78d6
Paulo Sousa <a89465@alunos.uminho.pt>
2020-05-15 16:53:33 +0100

    Menus interpretador

--- TrabalhoPratico/src/com/company/Interpretador.java
+++ TrabalhoPratico/src/com/company/Interpretador.java
@@ -4,15 +4,7 @@ import java.util.Scanner;
 
 public class Interpretador {
 
-    public void menu() {
-        System.out.println("Login");
-        System.out.println("Registar");
-        System.out.println();
-        System.out.println();
-        System.out.println();
-    }
-
-    public boolean login(Controlador c) {
+    public Login login(Controlador c) {
         Scanner s = new Scanner(System.in);
         String user, pass;
 
@@ -23,10 +15,15 @@ public class Interpretador {
             System.out.println("Introduza a password");
             pass = s.nextLine();
 
-            return c.containsPassword(user, pass);
+            s.close();
+
+            if(c.containsPassword(user, pass))
+                return c.getLogin(user);
         }
 
-        return false;
+        s.close();
+
+        return null;
     }
 
     public boolean registar(Controlador c) {
@@ -47,6 +44,8 @@ public class Interpretador {
             System.out.println("Introduza a password");
             pass = s.nextLine();
 
+            s.close();
+
             l.setPassword(pass);
 
             do { code = l.generateCode(tipo); } while(c.containsUser(code));
@@ -60,20 +59,142 @@ public class Interpretador {
             return true;
         }
 
+        s.close();
+
         return false;
+    }
+
+    public void menuUtilizador(Login l) {
+        boolean r=true;
+        Scanner s = new Scanner(System.in);
+        String line;
+
+        while(r) {
+            System.out.println("1 | Fazer uma encomenda");
+            System.out.println("2 | Aceder às encomendas anteriores");
+            System.out.println("Q | Sair");
+            line = s.nextLine();
+
+            switch(line) {
+                case "1":
+                    break;
+
+                case "2":
+                    break;
+
+                case "Q":
+                    r=false;
+                    break;
+
+                default:
+                    System.out.println("Comando inválido");
+            }
+        }
+    }
+
+    public void menuVoluntario(Login l) {
+        boolean r=true;
+        Scanner s = new Scanner(System.in);
+        String line;
+
+        while(r) {
+            System.out.println("1 | ");
+            System.out.println("Q | Sair");
+            line = s.nextLine();
+
+            switch(line) {
+                case "1":
+                    break;
+
+                case "Q":
+                    r=false;
+                    break;
+
+                default:
+                    System.out.println("Comando inválido");
+            }
+        }
+    }
+
+    public void menuTransportadora(Login l) {
+        boolean r=true;
+        Scanner s = new Scanner(System.in);
+        String line;
+
+        while(r) {
+            System.out.println("1 | ");
+            System.out.println("Q | Sair");
+            line = s.nextLine();
+
+            switch(line) {
+                case "1":
+                    break;
+
+                case "Q":
+                    r=false;
+                    break;
+
+                default:
+                    System.out.println("Comando inválido");
+            }
+        }
+    }
+
+    public void menuLoja(Login l) {
+        boolean r=true;
+        Scanner s = new Scanner(System.in);
+        String line;
+
+        while(r) {
+            System.out.println("1 | ");
+            System.out.println("Q | Sair");
+            line = s.nextLine();
+
+            switch(line) {
+                case "1":
+                    break;
+
+                case "Q":
+                    r=false;
+                    break;
+
+                default:
+                    System.out.println("Comando inválido");
+            }
+        }
+    }
+
+    public void menu(Login l) {
+        switch (l.getTipoConta()) {
+            case "Utilizador":
+                menuUtilizador(l);
+                break;
+            case "Voluntario":
+                menuVoluntario(l);
+                break;
+            case "Transportadora":
+                menuTransportadora(l);
+                break;
+            case "Loja":
+                menuLoja(l);
+                break;
+        }
     }
 
     public void interpretador(Controlador c) {
         boolean r=true;
         Scanner s = new Scanner(System.in);
         String line;
+        Login l = null;
 
         while(r) {
             System.out.println("Login ou Registar");
             line = s.nextLine();
 
             if(line.equals("Login") || line.equals("login")) {
-                if(login(c)) {
+                l = login(c);
+
+                if(l != null) {
                     r=false;
                     System.out.println("Login efetuado com sucesso");
                 }
@@ -96,5 +217,9 @@ public class Interpretador {
                 System.out.println("Comando Inválido");
             }
         }
+
+        s.close();
+
+        menu(l);
     }
 }
